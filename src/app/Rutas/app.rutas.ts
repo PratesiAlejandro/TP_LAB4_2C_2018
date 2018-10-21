34d0fb895e3f78843ca4210a1a65980e2d41ab7f
Alejandro <s_a_pr@hotmail.com>
2018-10-21 17:14:03 -0300

    Ruteo Componente Login, PaginaNoEncontrada , Home

---
+++ src/app/Rutas/app.rutas.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+// importo del module principal
+import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from '../componentes/login/login.component';
+import { HomeComponent } from '../componentes/home/home.component';
+import { PaginaNoEncontradaComponent} from '../componentes/pagina-no-encontrada/pagina-no-encontrada.component';
+
+
+
+// declaro donde quiero que se dirija
+const MiRuteo = [
+      {path: 'Login' , component: LoginComponent},
+    {path: 'Home' , component: HomeComponent},
+    { path: '',   redirectTo: '/Home', pathMatch: 'full' },
+    {path: '**' , component:PaginaNoEncontradaComponent}
+];
+
+@NgModule({
+    imports: [
+      RouterModule.forRoot(MiRuteo)
+    ],
+    exports: [
+      RouterModule
+    ]
+  })
+  export class Rutas { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,27 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppComponent } from './app.component';
+
+//RUTEO
+import { RouterModule, Routes } from '@angular/router';
+import { Rutas } from './Rutas/app.rutas';
+
+//COMPONENTES CREADOS 
+import { LoginComponent } from './componentes/login/login.component';
+import { HomeComponent } from './componentes/home/home.component';
+import { PaginaNoEncontradaComponent } from './componentes/pagina-no-encontrada/pagina-no-encontrada.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    HomeComponent,
+    PaginaNoEncontradaComponent
   ],
   imports: [
-    BrowserModule
-  ],
+    BrowserModule,
+    Rutas
+    ],
   providers: [],
   bootstrap: [AppComponent]
 })
